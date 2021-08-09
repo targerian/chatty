b2d1315df3de13cf0b06cbfa6a6f5eb12b3f455e
Cozmo <mohamed@lenovo.local>
2021-08-09 20:52:05 +0200

    first time deploying

--- src/components/SingOut.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../App.css";
-
-export const SignOut = ({ auth }) => {
-  return (
-    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
-  );
-};
-export default SignOut;
